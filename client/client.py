73ed35932603516f73dab87ec7a6ef046ce9e9b1
Prashant Dave <prashant.dave.email@gmail.com>
2021-01-02 02:36:02 -0500

    Added netshoot container and changed haproxy version

--- client/client.py
+++ client/client.py
@@ -4,10 +4,10 @@ import time
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger('urllib3')
 log.setLevel(logging.DEBUG)
-i = 8
-time.sleep(5)
+m = 10
+time.sleep(30)
 while True:
-    r = requests.get("http://haproxy/message/{}/{}".format(i*60, 1024))
+    r = requests.get("http://haproxy/message/{}/{}".format(m*60, 32))
     logging.info('%s %d', r, len(r.text))
     r.raise_for_status()
-    i = i + 1
+    m = m + 10
--- tcpdump.sh
+++ tcpdump.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker exec -u root -it haproxy-test_haproxy_1 tcpdump -vv -i eth0 -nn -X tcp
+# docker exec -u root -it haproxy-test_haproxy_1 tcpdump -K -vv -i eth0 -nn -X tcp
+docker run --rm -it --net container:haproxy-test_haproxy_1 nicolaka/netshoot tcpdump -K -i eth0 -nn -vv -X tcp
